35664a3f50084896f02cbfbd3f89dc1a05a394fc
INsiteLabDev <insitelabdev@gmail.com>
2020-04-14 12:19:47 -0700

    testing ssml

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft.util import play_mp3
 
 class HereComesTheSun(MycroftSkill):
     def __init__(self):
@@ -7,9 +7,12 @@ class HereComesTheSun(MycroftSkill):
 
     @intent_file_handler('sun.the.comes.here.intent')
     def handle_sun_the_comes_here(self, message):
-        self.speak_dialog('sun.the.comes.here')
+        # self.speak_dialog('sun.the.comes.here')
+        self.speak_dialog("<speak><prosody rate='10%'>" + "this is a test" + "</prosody></speak>")
+        play_mp3('/home/humaira/mycroft-skill/song.mp3')
 
 
 def create_skill():
     return HereComesTheSun()
+    
 
